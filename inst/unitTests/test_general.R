f6e6cfe2da9e6d8331329a8a46900a2d7aaa69a9
k.horan <k.horan@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-09-23 16:56:55 +0000

    fixed test that would fail if ChemmineOB was not installed
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ChemmineR@80660 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_general.R
+++ inst/unitTests/test_general.R
@@ -31,11 +31,13 @@ test.genAPDescriptors <- function(){
 
 }
 test.propOB <- function() {
-	data(sdfsample)
-	p = propOB(sdfsample[1:5])
-	print(p)
-	checkEquals(ncol(p),14)
-	checkEquals(nrow(p),5)
-   checkEquals(p$MW[2],MW(sdfsample[2])[[1]])
+	if(require(ChemmineOB)){
+		data(sdfsample)
+		p = propOB(sdfsample[1:5])
+		print(p)
+		checkEquals(ncol(p),14)
+		checkEquals(nrow(p),5)
+		checkEquals(p$MW[2],MW(sdfsample[2])[[1]])
+	}
 
 }
