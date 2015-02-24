06e599a540e32859ebe5b7ad8f66c68e0dbac8ff
Bioconductor Git-SVN Bridge <bioc-sync@bioconductor.org>
2015-02-24 12:08:33 -0800

    Commit made by the Bioconductor Git-SVN bridge.
    Consists of 1 commit.
    
    Commit information:
    
    SVN Revision number: 99769
    Commit message:
    force bridge to work
    Committed by d.tenenbaum
    Committed at: 2015-02-24T20:01:43.949361Z

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -2005,10 +2005,7 @@ groups <- function(x, groups="fctgroup", type="countMA") {
 	}
         groupMA <- sapply(names(groups), function(x) rowSums(neighbors[, rep(grep(groups[x], colnames(neighbors), perl=TRUE),2)]/2))
 	## Fix counts for ambiguous functional groups
-        if(c("ROR" %in% colnames(groupMA))) {
-	        groupMA[, "ROR"] <- groupMA[, "ROR"] - groupMA[, "RCOOR"]
-                groupMA[groupMA < 0] <- 0
-        }
+        if(c("ROR" %in% colnames(groupMA))) groupMA[, "ROR"] <- groupMA[, "ROR"] - groupMA[, "RCOOR"] 
 	if(mylength>1) {
                 return(groupMA)
         } else {
